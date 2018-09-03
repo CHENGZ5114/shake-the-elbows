ad8cb9ffba23b344c0cf8aad81ca44af0e6c262a
CHENGZ5114 <42401193+CHENGZ5114@users.noreply.github.com>
2018-09-03 15:15:15 +0800

    Add files via upload

--- die_visual.py
+++ die_visual.py
@@ -1,15 +1,29 @@
+import pygal
 from die import Die
-die=Die()
+die_1=Die()
+die_2=Die()
+
 results=[]
-for roll_num in range(100):
-	result=die.roll()
+for roll_num in range(1000):
+	result=die_1.roll()+die_2.roll()
 	results.append(result)
 	
 frequencies=[]
-for value in range(1,die.num_sides+1):
+max_result=die_1.num_sides+die_2.num_sides
+for value in range(1,max_result+1):
 	frequency=results.count(value)
 	frequencies.append(frequency)
 	
 print(results)
 print(frequencies)
+
+hist=pygal.Bar()
+
+hist.title='results of rolling two D6 1000 times'
+hist.x_labels=['1','2','3','4','5','6','7','8','9','10','11','12']
+hist.x_title='results'
+hist.y_title='frequency of result'
+
+hist.add('D6+D6',frequencies)
+hist.render_to_file('dice_visual.svg')
 
